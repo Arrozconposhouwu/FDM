bc6edf6bba360dd31aed1c1289d7ded9e29022b6
Arrozconposhouwu <yoelcomputadores3@gmail.com>
2023-08-07 10:47:07 -0400

    Galeria

--- src/js/app.js
+++ src/js/app.js
@@ -12,5 +12,36 @@ function IniciarAPP() {
 function CrearGaleria() {
     const galeria = document.querySelector('.galeria__imagenes'); // Obtiene el elemento con la clase 'galeria__imagenes' desde el DOM.
 
-    galeria.textContent = 'Texto de Prueba';
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/grande/${i}.avif" type="image/avif">
+            <source srcset="build/img/grande/${i}.webp" type="image/webp">
+            <img src="build/img/grande/${i}.png" alt="Imagen de Galeria" class="galeria__img">
+        `;
+
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen);
+    }
+}
+
+// Función que permite ver cada imagen de la galeria.
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img src="build/img/grande/${id}.png" alt="Imagen de Galeria" class="galeria__img">
+    `;
+
+    // Fondo negro.
+    const overlay = document.createElement('div');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    // Hacer que se muestre la imagen.
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
 }
